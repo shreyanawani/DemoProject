5f7052e2acfe375775f306a1f1210bb79eaf7d58
Shreya Nawani <shreya.nawani@BLRETV-N5UKG8WN.local>
2022-06-07 08:58:11 +0530

    this is java project

---
+++ src/com/flipkart/demo/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author shreya.nawani
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my java app");
+	}
+
+}
